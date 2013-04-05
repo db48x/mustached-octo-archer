a3a2124eea6544ce5ac566c40d988ec9ad02e9ba
Daniel Brooks <db48x@db48x.net>
2013-04-04 19:59:53 -0700

    big blob of generic rails gunk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scanner::Application.config.secret_token = '515a34bdf339505b2b158446cea91057bd80c05f57eb71693e6074ecd9c81793e9de84af4a947e31cf2a83f7a89c235611e29b7aa69d760342cbd132c0c62c3f'
