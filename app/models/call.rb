b698ef5d5c11ff45c575647698ce649c410211cd
Daniel Brooks <db48x@db48x.net>
2013-04-05 00:29:28 -0700

    Call start and end need to be dates, and must be in order. Show the talkgroup when listing calls, editing them, etc

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,9 +1,18 @@
 class Call < ActiveRecord::Base
-  attr_accessible :end, :frequency, :start
+  attr_accessible :end, :frequency, :start, :group_id
   belongs_to :group
   validates :start, :presence => true
   validates :end, :presence => true
+  validates :end, :date => { :after => :start }
   validates :frequency, :presence => true
+  validates :group_id,
+            :numericality => { :only_integer => true,
+                               :greater_than_or_equal_to => 0x0,
+                               :less_than_or_equal_to => 0xFFFF },
+                       :presence => true
   validates_associated :group
-  validates :group_id, :presence => true
+
+  def group_name
+    return group ? group.name : "UNKNOWN"
+  end
 end
